394a1f17b4887a51560b3ee27999e35764f92168
XAWalter <33841055+XAWalter@users.noreply.github.com>
2019-01-07 22:17:48 -0800

    Added functions to class

---
+++ Playlist.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include "Playlist.h"
+
+using namespace std;
+
+PlaylistNode::PlaylistNode(){
+
+
+}
+
+PlaylistNode::PlaylistNode(string, string, string, int, PlaylistNode*) {
+
+}
+
+void PlaylistNode::InsertAfter() {
+
+}
+
+void PlaylistNode::SetNext() {
+
+}
+
+string PlaylistNode::GetID() {
+
+}
+
+string PlaylistNode::GetSongName() {
+
+}
+
+string PlaylistNode::GetArtistName() {
+
+}
+
+int PlaylistNode::GetSongLength() {
+
+}
+
+PlaylistNode PlaylistNode::GetNext() {
+
+}
+
+void PlaylistNode::PrintPlaylistNode() {
+
+}
--- Playlist.h
+++ Playlist.h
@@ -1,1 +1,30 @@
-#pragma once
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+
+#include <iostream>
+
+using namespace std;
+
+class PlaylistNode{
+public:
+	PlaylistNode();
+	PlaylistNode(string, string, string, int, PlaylistNode*);
+	void InsertAfter();
+	void SetNext();
+	string GetID();
+	string GetSongName();
+	string GetArtistName();
+	int GetSongLength();
+	PlaylistNode GetNext();
+	void PrintPlaylistNode();
+
+private:
+	string uniqueID;
+	string songName;
+	string artistName;
+	int songLength;
+	PlaylistNode* nextNodePtr;
+
+};
+
+#endif
